679d3de6b96704ddaacaec336a99ff0204fbbb9d
assafzvigoren <assafzvigoren4@gmail.com>
2021-04-18 19:09:23 +0300

    app.listen

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello world!!');
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const app = express();
+const PORT = 9000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+})

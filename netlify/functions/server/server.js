df256aa5423852c02b04905213a039cbc37a1ba9
assafzvigoren <assafzvigoren4@gmail.com>
2021-04-27 20:05:14 +0300

    asdf

--- netlify/functions/server/server.js
+++
@@ -1,4 +0,0 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-const handler = {}
-
-module.exports = { handler }
--- src/function-server.js
+++
@@ -1,4 +0,0 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-import {handler} from './index'
-
-module.exports = { handler }

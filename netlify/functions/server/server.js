905035541ea70612843c4f6d8aabcca863a6846a
assafzvigoren <assafzvigoren4@gmail.com>
2021-04-27 20:00:37 +0300

    function

---
+++ netlify/functions/server/server.js
@@ -0,0 +1,4 @@
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const handler = {}
+
+module.exports = { handler }
---
+++ src/function-server.js
@@ -0,0 +1,4 @@
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+import {handler} from './index'
+
+module.exports = { handler }
